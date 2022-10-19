c5fec6ace9b7161e22e4511e9206ebacb097e3ff
ad <adrianleb@gmail.com>
2022-10-19 12:44:49 -0300

    put the rug in the carpet

--- components/Model.js
+++ components/Model.js
@@ -1,16 +1,21 @@
 import * as THREE from "three";
 import React, { useRef, useEffect } from "react";
-import { useAnimations, useGLTF } from "@react-three/drei";
+import { useAnimations, useGLTF, useTexture } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
 
-export function Model(props) {
+export function Model({imageUrl, ...props}) {
   const group = useRef(null);
   const { nodes, materials, animations, scene } = useGLTF(
     "/FlyingCarpet_test2.glb"
   );
 
+
+  const texture = useTexture(imageUrl);
   const { actions, mixer } = useAnimations(animations, group);
 
+
+  console.log(materials.Carpet.map, 'aaaaa')
+  materials.Carpet.map = texture;
   useEffect(() => {
     actions.Action?.play();
   }, [mixer]);
