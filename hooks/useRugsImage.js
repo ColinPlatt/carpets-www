2864a09c8acd33b65c1198b10abd1e9156f59a0e
Colin Platt <colingplatt@gmail.com>
2022-10-19 15:17:30 +0200

    adding random selection logic

--- hooks/useRugsImage.js
+++ hooks/useRugsImage.js
@@ -23,21 +23,38 @@ export function useRugsImage(connectedAccount) {
         }
     };
 
-    let output;
+    // get the token IDs held by the user
+
+    let rugsResults;
     
     axios
     .request(options)
     .then(function (response) {
         console.log(response.data.result[0].token_uri);
 
-        output = response.data.result[0].token_uri
+        output = response.data.rugsResults;
         
     })
     .catch(function (error) {
         console.error(error);
     });
 
-    return `${output || 0}`
+    let selected_id;
+
+    if(rugsResults.length != 0) {
+        if{rugsResults.length > 1} {
+            selected_id = Math.floor(Math.random()*rugsResults.length)
+        } else {
+            selected_id = response.data.result[0].token_id;
+        }
+    } else {
+        return '0'; //there is no token ID 0
+    }
+
+    return selected_id;
+    
+
+    
 }
 
 export default useRugsImage;
